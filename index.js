22a06887ec1ff0db79b5a4dce98d5ab055aea836
Alex Corn <mckay@doctormckay.com>
2018-12-20 00:58:47 -0500

    Fixed signature check invalidating encrypted app tickets

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 exports.parseAppTicket = require('./components/parseAppTicket');
 exports.parseEncryptedAppTicket = require('./components/parseEncryptedAppTicket');
-exports.parseEncryptedAppTicketEx = require('./components/parseEncryptedAppTicketEx');
