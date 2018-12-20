fcac6cec4764c592e83e2ad1dae3bf78b7fb89a9
Alex Corn <mckay@doctormckay.com>
2018-12-20 00:52:57 -0500

    Remove unnecessary properties from decrypted app tickets

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.parseAppTicket = require('./components/parseAppTicket');
 exports.parseEncryptedAppTicket = require('./components/parseEncryptedAppTicket');
+exports.parseEncryptedAppTicketEx = require('./components/parseEncryptedAppTicketEx');
